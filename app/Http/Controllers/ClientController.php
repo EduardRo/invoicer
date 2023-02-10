e3240a1a743b88195162e00b125827bf14cdb463
EduardRo <eduardstancu@yahoo.com>
2023-02-10 22:29:54 +0200

    error database

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ClientController extends Controller
 {
     //
     public function index(){
-        return view('InsertClient');
+
+       $clients=Client::all();
+        return $clients->toJson();
+        //return view('InsertClient',['title'=>'lsdfjsldfjkl']);
     }
 
 }
--- resources/views/InsertClient.blade.php
+++ resources/views/InsertClient.blade.php
@@ -25,6 +25,10 @@
    </head>
    <body class="antialiased">
     <p>This is client</p>
+    <div>
+        <h1>{{ $title }}</h1>
+    
+    </div>
        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         
 
